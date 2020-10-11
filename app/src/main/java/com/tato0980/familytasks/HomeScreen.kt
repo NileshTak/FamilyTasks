52b61262de6f7d6d9bd41166860b5a00ceb270f3
Nilesh Tak <me.nileshtak@gmail.com>
2020-10-11 14:21:09 +0530

    AUth

--- app/src/main/java/com/tato0980/familytasks/HomeScreen.kt
+++ app/src/main/java/com/tato0980/familytasks/HomeScreen.kt
@@ -3,9 +3,9 @@ package com.tato0980.familytasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LottieAnim : AppCompatActivity() {
+class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lottie_anim)
+        setContentView(R.layout.activity_home_screen)
     }
 }
--- app/src/main/java/com/tato0980/familytasks/MainActivity.kt
+++ app/src/main/java/com/tato0980/familytasks/MainActivity.kt
@@ -1,21 +1,73 @@
 package com.tato0980.familytasks
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var rvSigninGoogle : RelativeLayout
+
+    val RC_SIGN_IN = 1000
+
+    var googleSignInClient : GoogleSignInClient? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvSigninGoogle = findViewById<RelativeLayout>(R.id.rvSigninGoogle)
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        rvSigninGoogle.setOnClickListener {
+            var signInIntent = googleSignInClient?.signInIntent
+            startActivityForResult(signInIntent,RC_SIGN_IN)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        moveNextPage()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            var task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            var account = task.getResult(ApiException::class.java)
+            firebaseAuthWithGoogle(account)
+        }
+    }
+
+    fun firebaseAuthWithGoogle(acct : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(acct?.idToken,null)
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                task ->
+            if(task.isSuccessful){
+                moveNextPage()
+            }
+        }
+    }
+
+    fun moveNextPage(){
+        var currentUser = FirebaseAuth.getInstance().currentUser
+        if(currentUser != null){
+            startActivity(Intent(this,HomeScreen::class.java))
+        }
     }
 }
-
-
-
-//no. screens
-
-//login
-//signup
-//homescreen
-//design screens
-//structure of db
